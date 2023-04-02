556ede9a29b685aef639806200c6893c794d4e82
hokushi <hokushi97@gmail.com>
2023-04-02 13:13:38 +0900

    ssrでapi実行

--- src/pages/react-basic/basic14.tsx
+++ src/pages/react-basic/basic14.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Basic14 = ({ initialWidth }) => {
+const Basic14 = ({ initialWidth,todos}) => {
   const [number, setNumber] = useState(1);
   const [width, setWidth] = useState(initialWidth); // SSRの問題を引き起こす
 
   useEffect(() => {
     setNumber(2);
     setWidth(window.innerWidth);
-  }, []);
+    console.log(todos)},
+    []);
 
   return (
     <>
@@ -18,16 +20,27 @@ const Basic14 = ({ initialWidth }) => {
   );
 };
 
-export function getServerSideProps() {
+export async function getServerSideProps() {
     // サーバーサイドでデータを取得し、初期幅を設定
     const initialWidth = 0; // ここではデフォルト値として0を使用
+  
+    const URL = "https://jsonplaceholder.typicode.com/todos";
+    let todos = [];
+  
+    try {
+      const res = await axios.get(URL);
+      todos = res.data;
+    } catch (err) {
+      console.log(err);
+    }
   
     return {
       props: {
         initialWidth,
+        todos,
       },
     };
   }
-  
+
   export default Basic14;
 
