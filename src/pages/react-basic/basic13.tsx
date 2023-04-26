841cc8eb1fb04ed26c1c4452fc3d937ea66110b2
hokushi <hokushi97@gmail.com>
2023-04-26 13:19:25 +0900

    一旦コミット

--- src/pages/react-basic/basic13.tsx
+++ src/pages/react-basic/basic13.tsx
@@ -11,12 +11,11 @@ const Home = () => {
 
   //atomの値の操作を定義（read,write)
   const [count, setCount] = useRecoilState(countState);
-  const [user, setUser] = useRecoilState(userState);
   const pull = useRecoilValue(pullState);
+  const [user, setUser] = useRecoilState(userState);
 
-  //count increment処理
-  const increment = c => {
-    return c + 1;
+  const increment =() => {
+    setCount(count + 1);
   }
 
   //userのageをカウントアップ（機能としては意味無し）
@@ -26,23 +25,28 @@ const Home = () => {
 
   return (
     <>
-      <h1>Home</h1>
+      <p>まずrecoilを使うためにはrecoilを使う際に適応するべき場所を＜recoilroot＞で囲む</p>
+      <p>今回はcomponentフォルダのatoms.tsxにてstateを管理する</p>
+      <p>そこで管理するstateをどこでも使うようにできるのがrecoilのイメージ</p>
+      <p className="text-red-500 mt-5">基本的な使い方</p>
       <div>
         <p>count:{count}</p>
-        <button onClick={() => setCount(increment)}>count increment</button>
-        <hr />
+        <button onClick={increment}>count increment</button>
       </div>
-      <MyComponentWithPersistAtom/>
-      <div>
-        <Link href="/about13">About</Link>
-      </div>
+      <p>これはuseRecoilStateを使いstateの値、更新関数を参照した</p>
       <div className="mt-10">
+        <p>次はもっと簡単にuseRecoilValueを利用してstate値を利用するだけ</p>
         <p>{pull.title}</p>
         <p>{pull.name}</p>
         <p>{pull.age}</p>
         <p>{pull.postnumber}</p>
         <p>{pull.address}</p>
         <p>{pull.tel}</p>
+      </div>
+      <div className="mt-10 text-red-500">難しめ</div>
+      <MyComponentWithPersistAtom/>
+      <div>
+        <Link href="/about13">About</Link>
       </div>
     </>
   );
