7fd84286ee68075d86d1fe9ce9240f13e0af8a7f
hokushi <hokushi97@gmail.com>
2023-04-28 14:26:28 +0900

    文の訂正

--- src/pages/react-basic/basic13.tsx
+++ src/pages/react-basic/basic13.tsx
@@ -23,6 +23,7 @@ const Home = () => {
   return (
     <>
       <p>まずrecoilを使うためにはrecoilを使う際に適応するべき場所を＜recoilroot＞で囲む</p>
+      <p>今回はページ全体につけるためにapp.tsxですべてを囲っている</p>
       <p>今回はcomponentフォルダのatoms.tsxにてstateを管理する</p>
       <p>そこで管理するstateをどこでも使うようにできるのがrecoilのイメージ</p>
       <p className="text-red-500 mt-5">基本的な使い方</p>
@@ -42,6 +43,7 @@ const Home = () => {
       </div>
       <div className="mt-10 text-red-500">難しめ</div>
       <p>persistatomを使いstateの永続化をしたも。しかしrecoilpersistはライアントサイドでしか動かない。そのためサーバー側とクライアント側でレンダリングする内容に違いが生じる</p>
+      <p>おそらくrecoilpersistはlocalstrageにて保存をしているためssrとの共存が不可なのではないかと</p>
       <p>解決方法を表示する。</p>
       <p>dynamicを使うことでssrを実行しないようにしている</p>
       <MyComponentWithPersistAtom/>
